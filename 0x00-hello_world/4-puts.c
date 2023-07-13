16b0af70a9e96c8228f985660e061b0443aa88d7
Hossam <hossam136dehhaoui@gmail.com>
2023-07-13 18:21:21 -0400

    done task 4²

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Description: 'this is the desc'
+ *
+ *
+ * Return: always 0 (sucess)
+ *
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

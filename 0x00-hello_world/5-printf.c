e6e4a773295c222a37fbc011c592afa00953878f
Hossam <hossam136dehhaoui@gmail.com>
2023-07-13 19:03:34 -0400

    done task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'this is the desc'
+ *
+ *
+ * Return: always 0 (sucess)
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

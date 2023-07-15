227e6ae3b941085b0215ed29a604f0d26b1a1c70
Hossam <hossam136dehhaoui@gmail.com>
2023-07-15 10:10:53 -0400

    task 2 done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  * main - "entry"
+  *
+  * Description - 'Hey'
+  *
+  * Return: 0 (success)
+  *
+  */
+
+
+int main(void)
+{
+	int start = 'a';
+	int end = 'z';
+	int i;
+
+	for (i = start; i <= end; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

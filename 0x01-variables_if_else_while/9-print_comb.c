ac04f222e2b154a3ea6d2162b256661beadb1efb
Hossam <hossam136dehhaoui@gmail.com>
2023-07-15 12:40:17 -0400

    task 9 done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+  * main - "entry"
+  * Description - 'Hey'
+  * Return: 0 (success)
+  *
+  *
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

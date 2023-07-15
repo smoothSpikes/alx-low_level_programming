eb3738961a0af097359f66a69f6a16026da59a3c
Hossam <hossam136dehhaoui@gmail.com>
2023-07-15 12:19:57 -0400

    task 8 done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+  * main - "entry"
+  * Description - 'Hey'
+  * Return: 0 (success)
+  *
+  *
+  */
+
+
+int main(void)
+{
+	int i;
+	int s = 'a';
+	int e = 'f';
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (i = s; i <= e; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+}

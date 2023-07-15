9aad35bb9bc335e4d0fbdd5af222b9c1f00c2537
Hossam <hossam136dehhaoui@gmail.com>
2023-07-15 11:30:47 -0400

    task 4 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  * main - "entry"
+  *
+  * Description - 'Hey'
+  *
+  * Return: 0 (success)
+  *
+  */
+
+
+int main(void)
+{
+	int start = 'a';
+	int end = 'z';
+	int i;
+
+	for (i = start; i <= end; i++)
+	{
+		if ( (i != 'q') && (i != 'e' ))
+		{
+		putchar(i);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

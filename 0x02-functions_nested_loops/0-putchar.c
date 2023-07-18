0a48cfbe81b4fe2ed5b55a9dca8bde050d6a9a3e
Hossam <hossam136dehhaoui@gmail.com>
2023-07-18 16:20:18 -0400

    done task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+  * main - "entry"
+  * Description - 'Hey'
+  * Return: 0 (success)
+  *
+  *
+  */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}

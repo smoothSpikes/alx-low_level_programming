61472b4025910459e95e3d9bb18e9c03b49842cc
Hossam <hossam136dehhaoui@gmail.com>
2023-07-18 19:13:41 -0400

    task 2 done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * print_alphabet_x10 - "entry"
+  * Description - 'Hey'
+  * Return: 0 (success)
+  *
+  *
+  */
+
+
+void print_alphabet_x10 (void)
+{
+	char a;
+
+	for (a = 1; a <=10; a++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			putchar(a);
+		}
+		putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -11,3 +11,9 @@ int main(void)
     print_alphabet();
     return 0;
 }
+
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}

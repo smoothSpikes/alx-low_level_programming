94862b1d8a3918711e18613cd06601c8893df6ae
Hossam <hossam136dehhaoui@gmail.com>
2023-07-19 07:18:02 -0400

    task 6 done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * _abs - "computes abs value"
+  * @r: is the int to be checked
+  * Return: 0
+  *
+  *
+  */
+
+
+int _abs(int)
+{
+	printf(abs(int));
+	return (0);
+}

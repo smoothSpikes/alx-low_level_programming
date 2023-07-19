1005dec67971839c306b2f0813dc7d3fdb1f4c57
Hossam <hossam136dehhaoui@gmail.com>
2023-07-19 06:23:06 -0400

    task 4 done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,16 @@
 #include "main.h"
 
+/**
+  * _isalpha - checks for alphanum characters
+  *
+  * @c : is the input char
+  *
+  * Return: 1 if found 0 otherwise
+  */
+
 
 
 int _isalpha(int c)
 {
-	return((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }

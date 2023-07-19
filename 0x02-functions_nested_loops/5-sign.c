9aabc2cea9f1e4c607d4409d8e1cdb1ad1108b07
Hossam <hossam136dehhaoui@gmail.com>
2023-07-19 06:41:36 -0400

    task 5 done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  * print_sign - checks for int sign
+  * @n: ckacked int
+  * Return: "1" if +, "0" if "0", "-1" if +
+  *
+  */
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	else if (n < 0)
+	{
+		printf("-1");
+		return (-1);
+	}
+
+
+}

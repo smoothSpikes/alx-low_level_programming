ad3e678fef47ebf032f27f44255531ac7f3ad824
Hossam <hossam136dehhaoui@gmail.com>
2023-07-21 08:29:22 -0400

    task 5 done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * more_numbers - prints more numbers
+  *
+  *
+  */
+
+void more_numbers(void)
+{
+	int i;
+	int r;
+
+	for (i = 0; i <= 10; )
+	{
+		for (r = 0; r <= 14; r++)
+		{
+			putchar(r + '0');
+		}
+
+		i++;
+
+		putchar('\n');
+	}
+
+	putchar('\n');
+}
+

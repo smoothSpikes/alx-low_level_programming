9dcf8ad4be8b113861e175b48c2d8c4c622a734f
Hossam <hossam136dehhaoui@gmail.com>
2023-07-21 08:00:45 -0400

    task 3 done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints nums from 0 to 9
+  *
+  *
+  *
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+}

ecf7082870d309d53175641f32d71e654b894bb5
Hossam <hossam136dehhaoui@gmail.com>
2023-07-21 07:33:36 -0400

    task 1 one

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _isdigit - checks if a char is digit
+  * @c : char to check
+  * Return: 1 if digit,0 otherwise
+  *
+  *
+  */
+
+int _isdigit(int c)
+{
+	for (c = 0; c >= 9; c++)
+	{
+		if (isdigit(c))
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
